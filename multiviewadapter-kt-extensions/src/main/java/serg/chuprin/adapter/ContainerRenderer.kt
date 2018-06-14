f838d287094b786d901ec4af98c7914aea810168
Сергей <gregamer@gmail.com>
2018-06-15 00:39:22 +0300

    fix

--- multiviewadapter-kt-extensions/src/main/java/serg/chuprin/adapter/ContainerRenderer.kt
+++ multiviewadapter-kt-extensions/src/main/java/serg/chuprin/adapter/ContainerRenderer.kt
@@ -1,8 +1,5 @@
 package serg.chuprin.adapter
 
-import android.support.annotation.LayoutRes
-import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 
 abstract class ContainerRenderer<in M : Any> : ViewRenderer<M, ContainerHolder>() {
@@ -13,9 +10,5 @@ abstract class ContainerRenderer<in M : Any> : ViewRenderer<M, ContainerHolder>(
     ): ContainerHolder {
         return ContainerHolder(inflate(type, parent))
                 .also { holder -> onVhCreated(holder, clickListener, longClickListener) }
-    }
-
-    private fun inflate(@LayoutRes layout: Int, parent: ViewGroup): View {
-        return LayoutInflater.from(parent.context).inflate(layout, parent, false)
     }
 }
